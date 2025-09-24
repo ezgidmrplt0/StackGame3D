0c222115e8acdaa99182e17ba382409fc444d60d
verNax <96057859+verNax34@users.noreply.github.com>
2025-09-24 16:58:49 +0300

    ana menü

---
+++ Assets/Scripts/MainMenu/Menu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Ayarlar sahnesine geçiþ metodu
+    public void Ayarlar()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    // Ana menüye geri dönme metodu
+    public void GeriDon()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
